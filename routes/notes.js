1f53ba3799c0b36a1f1d49079e725a1efe7f88b8
Sonika21-rastogi <rastogi.sonika1999@gmail.com>
2025-08-19 20:54:27 +0530

    Create Notes in Models

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const FetchUser  = require('../middleware/FetchUser');
+const Notes = require('../models/Notes');
 
-router.get('/', (req, res) => {
-    
-    res.json([]);
-})
+router.get('/fetchallnotes', FetchUser, async(req, res) => {
+    const notes = await Notes.find({user: req.user});
+    res.json(notes)
+});
 
 module.exports = router;    
